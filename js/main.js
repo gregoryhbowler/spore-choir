88414e939157c61a3284c804cba1dbdbd21679ef
Gregory Bowler <gregoryhbowler@users.noreply.github.com>
2025-11-23 18:01:10 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { SporeChoirUI } from './ui.js';
+
+// Initialize application when DOM is loaded
+let ui;
+window.addEventListener('DOMContentLoaded', () => {
+    ui = new SporeChoirUI();
+});
